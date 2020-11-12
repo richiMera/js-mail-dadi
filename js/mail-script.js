c1ef9c53eb9efb3d2c3fc0de5fcac58f42e03500
Riccardo Ferrari <richi.f93@gmail.com>
2020-11-12 17:08:30 +0100

    ci siamo quasi

---
+++ js/mail-script.js
@@ -0,0 +1,18 @@
+var mailList = ["nico@gmail.com", "mara@gmail.com", "cabel@gmail.com", "richi@gmail.com"];
+console.log(mailList);
+var userMail = prompt("inserisci la tua mail");
+console.log(userMail);
+var access = false;
+for (var i = 0; i < mailList.length; i++) {
+  console.log(mailList[i]);
+  if (userMail === mailList) {
+    access = true;
+    console.log(access);
+  }
+}
+
+if (access === true) {
+  document.getElementById("allowed_denied").innerHTML = "Accesso Consentito";
+} else {
+  document.getElementById("allowed_denied").innerHTML = "Accesso Non Consentito";
+}
--- js/script.js
+++ js/script.js
@@ -2,18 +2,22 @@
 var numbers = [Math.floor(Math.random() * 7), Math.floor(Math.random() * 7)]
 console.log(numbers);
 var message = "";
-for (var i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
+var userNumber = numbers[0]
+console.log(userNumber);
+var pcNumber = numbers[1]
+console.log(pcNumber);
+for (var i = 0; i < numbers.length; i++)
 
-  if (numbers[0] > numbers[1]) {
-    console.log(message = "il vincitore è l'utente");
-  } else if (numbers[0] < numbers[1]) {
-    console.log(message = "il vincitore è il pc");
-  }
-  else {
-    console.log(message = "pareggio");
-  }
+if (userNumber > pcNumber) {
+  console.log("user vince");
+} else if (userNumber < pcNumber) {
+  console.log("pc vince");
 }
+else {
+  console.log("pareggio");
+}
+
+
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
